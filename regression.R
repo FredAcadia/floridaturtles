563ccc66b35628fc966264f43f043213116c2814
Frederic Viennot <fredviennot@gmail.com>
2023-04-25 23:33:34 -0400

    Update regression code and data

--- regression.R
+++ regression.R
@@ -1,18 +1,49 @@
 # get work directory path with getwd() then set
-setwd("/Users/fredericviennot/Documents/Perso/Unity/MATH 401/floridaturtles")
+setwd("C:/Users/Fredo/Desktop/Unity/MATH 401/floridaturtles")
 
-greenturtle.nesting.data <- read.csv("data/greenturtlenestingdata5years.csv", header = T, sep = ",")
-
-greenturtle.nesting.year <- c(2018,2019,2020,2021,2022)
+#Green turtle
+greenturtle.nesting.data <- read.csv("data/greenturtletotals.csv", header = T, sep = ",")
+greenturtle.nesting.year <- greenturtle.nesting.data$Year
 greenturtle.nesting.year.n <- greenturtle.nesting.year - 2018
-greenturtle.nesting.count <- c(4546,53016,26656,32680,37028)
+greenturtle.nesting.count <- greenturtle.nesting.data$Count
 
-#Create an scatter plot of year vs. count
-plot(greenturtle.nesting.year,greenturtle.nesting.count, xlab="Year",ylab="Count",pch=16,cex.lab=1.5,cex.axis=1)
+#Loggerhead
+loggerhead.nesting.data <- read.csv("data/loggerheadturtletotals.csv", header = T, sep = ",")
+loggerhead.nesting.year <- loggerhead.nesting.data$Year
+loggerhead.nesting.year.n <- loggerhead.nesting.year - 2018
+loggerhead.nesting.count <- loggerhead.nesting.data$Count
 
-#Fit the model, log(count) = b0 + b1*year using possion distribution 
-#The output is saved in an object scrub.jay
-scrub.jay<-glm(greenturtle.nesting.count~greenturtle.nesting.year.n, family=poisson)
+#Leatherback
+leatherback.nesting.data <- read.csv("data/leatherbackturtletotals.csv", header = T, sep = ",")
+leatherback.nesting.year <- leatherback.nesting.data$Year
+leatherback.nesting.year.n <- leatherback.nesting.year - 2018
+leatherback.nesting.count <- leatherback.nesting.data$Count
+
+#All turtles
+allturtles.nesting.data <- read.csv("data/allturtlestotals.csv", header = T, sep = ",")
+allturtles.nesting.year <- allturtles.nesting.data$Year
+allturtles.nesting.year.n <- allturtles.nesting.year - 2018
+allturtles.nesting.count <- allturtles.nesting.data$Count
+
+#Models
+greenturtle_model <- glm(greenturtle.nesting.count~greenturtle.nesting.year.n, family = poisson)
+loggerhead_model <- glm(loggerhead.nesting.count~loggerhead.nesting.year.n, family = poisson)
+leatherback_model <- glm(leatherback.nesting.count~leatherback.nesting.year.n, family = poisson)
+allturtles_model <- glm(allturtles.nesting.count~allturtles.nesting.year.n, family = poisson)
+
+#Plots
+par(mfrow=c(2,2))
+plot(greenturtle.nesting.year,greenturtle.nesting.count, xlab="Year",ylab="Count",pch=16,cex.lab=1.5,cex.axis=1, main="Green Turtle nesting count per year")
+lines(greenturtle.nesting.year, fitted(greenturtle_model))
+plot(loggerhead.nesting.year,loggerhead.nesting.count, xlab="Year",ylab="Count",pch=16,cex.lab=1.5,cex.axis=1, main="Loggerhead nesting count per year")
+lines(loggerhead.nesting.year, fitted(loggerhead_model))
+plot(leatherback.nesting.year,leatherback.nesting.count, xlab="Year",ylab="Count",pch=16,cex.lab=1.5,cex.axis=1, main="Leatherback nesting count per year")
+lines(leatherback.nesting.year, fitted(leatherback_model))
+plot(allturtles.nesting.year, allturtles.nesting.count, xlab="Year",ylab="Count",pch=16,cex.lab=1.5,cex.axis=1, main="All turtles nesting count per year" )
+lines(allturtles.nesting.year, fitted(allturtles_model))
 
 #Display the results of the parameter estimates and associated statistics
-summary(scrub.jay)
+summary(greenturtle_model)
+summary(loggerhead_model)
+summary(leatherback_model)
+summary((allturtles_model))
